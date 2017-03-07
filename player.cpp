c60ee278c02b808cf17ceb922dcd4fef0811f362
David Elliott <elliott.david.ballantyne@gmail.com>
2017-03-06 22:14:50 -0800

    Working minimax

--- player.cpp
+++ player.cpp
@@ -13,11 +13,6 @@ Player::Player(Side side_in)
       side(side_in),
       testingMinimax(false)   // Will be set to true in test_minimax.cpp.
 {
-    /*
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
 }
 
 /*
@@ -28,12 +23,17 @@ Player::~Player()
     delete board;
 }
 
+void Player::set_board(Board* board_in)
+{
+    board = board_in;
+}
+
 double Player::minimax(Board* board, int depth, bool maximizing)
 {
     double score = 0.0;
     if(depth == 0)
     {
-        score = heuristic(board, side);
+        score = parity_heuristic(board, side);
         std::cerr << "Heuristic: " << score << std::endl;
     }
     else
@@ -65,12 +65,22 @@ double Player::minimax(Board* board, int depth, bool maximizing)
     return score;
 }
 
+/**
+ * @brief Returns the difference between move_side's pieces and the other
+ * side's pieces.
+ */
 int Player::parity_heuristic(Board* board, Side move_side)
 {
     int ret = board->count(move_side) - board->count(OTHER_SIDE(move_side));
     return ret;
 }
 
+/**
+ * @brief Return value is incremented by 1 for every move_side edge piece,
+ * incremented by 2 for every move_side corner piece, decremented by 1 for
+ * move_side pieces 1 away from the edge, and the opposite for the other side's
+ * pieces.
+ */
 int Player::edge_heuristic(Board* board, Side move_side)
 {
     int ret = 0;
@@ -82,30 +92,58 @@ int Player::edge_heuristic(Board* board, Side move_side)
         for(int j = 0; j < 8; j += 7)
         {
             if(board->get(move_side, i, j))
-                ret++;
+                ret+=1;
             else if(board->get(OTHER_SIDE(move_side), i, j))
-                ret--;
+                ret-=1;
         }
     }
 
     for(int i = 1; i < 7; i++)
     {
-        for(int j = 1; j < 7; j+= 6)
+        for(int j = 1; j < 7; j += 6)
         {
             if(board->get(move_side, i, j))
-                ret--;
+                ret-=1;
             else if(board->get(OTHER_SIDE(move_side), i, j))
-                ret++;
+                ret+=1;
         }
     }
+
 
     return ret;
 }
 
+int Player::mobility_heuristic(Board* board, Side move_side)
+{
+    int n_moves = 0;
+    int n_opponent_moves = 0;
+    Move move(0, 0);
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            move.set_x(i);
+            move.set_y(j);
+            if(board->checkMove(&move, move_side))
+                n_moves++;
+            if(board->checkMove(&move, OTHER_SIDE(move_side)))
+                n_opponent_moves++;
+        }
+    }
+    return n_moves - n_opponent_moves;
+}
+
+/**
+ * @brief Returns a weighted sum of all heuristics.
+ */
 double Player::heuristic(Board* board, Side move_side)
 {
-    return 1.0 * parity_heuristic(board, move_side) + 2.0 * edge_heuristic(board, move_side);
+    return 1.0 * parity_heuristic(board, move_side); + 
+        testingMinimax ? 0.0 : 
+        ( 5.0 * edge_heuristic(board, move_side) +
+          5.0 * mobility_heuristic(board, move_side) );
 }
+
 /*
  * Compute the next move given the opponent's last move. Your AI is
  * expected to keep track of the board on its own. If this is the first move,
@@ -123,12 +161,34 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 {
     board->doMove(opponentsMove, OTHER_SIDE(side));
 
-    double best = minimax(board, 4, true);
+    bool found_move = false;
+    std::pair<double, Move> best_move(0.0, Move(0, 0));
+    Move move(0, 0);
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            move.set_x(i);
+            move.set_y(j);
+            if(board->checkMove(&move, side))
+            {
+                double score = minimax(board, 1, true);
+                if(!found_move || score > best_move.first)
+                {
+                    found_move = true;
+                    best_move.first = score;
+                    best_move.second = move;
+                }
+            }
+        }
+    }
 
-    std::cerr << "best: " << best << std::endl;
+    Move* ret = nullptr;
+    if(found_move)
+    {
+        board->doMove(&best_move.second, side);
+        ret = new Move(best_move.second);
+    }
 
-    //if(ret)
-        //board->doMove(ret, side);
-
-    return nullptr;
+    return ret;
 }
--- player.hpp
+++ player.hpp
@@ -13,17 +13,29 @@ private:
     Side side;
 
 public:
+    
+    // Constructors / Destructors
     Player(Side side_in);
     ~Player();
 
-    double minimax(Board* board, int depth, bool maximizing);
+    // Mutators
+    void set_board(Board* board_in);
+    void set_side(Side side_in);
 
+    // Accessors
+    Board* get_board();
+    Side get_side();
+
+    // Heuristics
     int parity_heuristic(Board* board, Side move_side);
     int edge_heuristic(Board* board, Side move_side);
+    int mobility_heuristic(Board* board, Side move_side);
     double heuristic(Board* board, Side move_side);
 
-//    std::pair<double, Move*> minimax(Board* board, int depth, Side max_side, bool maximizing);
+    // Minimax
+    double minimax(Board* board, int depth, bool maximizing);
 
+    // Game code
     Move *doMove(Move *opponentsMove, int msLeft);
 
     // Flag to tell if the player is running within the test_minimax context
